99b603e138e700f9d0314f75549024352005b95e
Pritiranjan <patrapritiranjan11@gmail.com>
2024-05-17 11:06:00 +0530

    first assignment done

---
+++ index.js
@@ -0,0 +1,12 @@
+let box=document.getElementById("box");
+box.style.backgroundColor="yellow";
+box.style.color="blue";
+box.style.margin='20px';
+box.style.padding='20px';
+box.style.fontSize='18px';
+box.style.fontWeight="bold";
+box.style.height='200px';
+box.style.width='300px';
+
+
+
